3a6072bf5ea4fee38680bd8152d3f84e66dd48fb
Oscar Servellon <rockerolife66@gmail.com>
2020-05-25 04:15:13 -0700

    5/25/20 finished writing the code but there seems to be a probelm where the import of 'Levels' is not working for neither mine, of the authors code, when i try them.

--- python/main.py
+++ python/main.py
@@ -80,4 +80,97 @@ def startLevelGame(level, screen, font):
 				ghost.changeSpeed(ghost.tracks[ghost.tracks_loc[0]][0: 2])
 				ghost.tracks_loc[1]+= 1
 			else:
-				if ghost.tracks_loc
+				if ghost.tracks_loc[0]< len(ghost.tracks)- 1:
+					ghost.tracks_loc[0]+= 1
+				elif ghost.role_name== 'Clyde':
+					ghost.tracks_loc[0]= 2
+				else:
+					ghost.tracks_loc[0]= 0
+				ghost.changeSpeed(ghost.tracks[ghost.tracks_loc[0]][0: 2])
+				ghost.tracks_loc[1]= 0
+			if ghost.tracks_loc[1]< ghost.tracks[ghost.tracks_loc[0]][2]:
+				ghost.changeSpeed(ghost.tracks[ghost.tracks_loc[0]][0: 2])
+			else:
+				if ghost.tracks_loc[0]< len(ghost.tracks)- 1:
+					loc0= ghost.tracks_loc[0]+ 1
+				elif ghost.role_name== 'Clyde':
+					loc0= 2
+				else:
+					loc0= 0
+				ghost.changeSpeed(ghost.tracks[loc0][0: 2])
+			ghost.update(wall_sprites, None)
+		ghost_sprites.draw(screen)
+		score_text= font.render("Score: %s" % SCORE, True, RED)
+		screen.blit(score_text, [10, 10])
+		if len(food_sprites)== 0:
+			is_clearance= False
+			break
+		pygame.display.flip()
+		clock.tick(10)
+	return is_clearance
+
+
+
+def showText(screen, font, is_clearance, flag=False):
+	clock= pygame.time.Clock()
+	msg= 'Game Over!' if not is_clearance else 'Congratulations, you won!'
+	positions= [[234, 233], [65, 303], [170, 333]] if not is_clearance else [[145, 233], [65, 303], [170, 333]]
+	surface= pygame.Surface((400, 200))
+	surface.set_alpha(10)
+	surface.fill((128, 128, 128))
+	screen.blit(surface, (100, 200))
+	texts= [font.render(msg, True, WHITE),
+			font.render('Press ENTER to continue or play again.', True, WHITE),
+			font.render('Press ESCAPE to quit.', True, WHITE)]
+	while True:
+		for event in pygame.event.get():
+			if event.type== pygame.QUIT:
+				sys.exit()
+				pygame.quit()
+			if event.type== pygame.KEYDOWN:
+				if event.key== pygame.K_RETURN:
+					if is_clearance:
+						if not flag:
+							return
+						else:
+							main(initialize())
+					else:
+						main(initialize())
+				elif event.key== pygame.K_ESCAPE:
+					sys.exit()
+					pygame.quit()      #NOTE: creating Escape Option, Exit
+		for idx, (text, position) in enumerate(zip(texts, positions)):
+			screen.blit(text, position)
+		pygame.display.flip()
+		clock.tick(10)
+
+
+def initialize():
+	pygame.init()
+	icon_image= pygame.image.load(ICONPATH)
+	pygame.display.set_icon(icon_image)
+	screen= pygame.display.set_mode([606, 606])
+	pygame.display.set_caption('Pacman-微信公众号Charles的皮卡丘')
+	return screen
+
+
+
+def main(screen):
+	pygame.mixer.init()
+	pygame.mixer.music.load(BGMPATH)
+	pygame.mixer.music.play(-1, 0.0)
+	pygame.font.init()
+	font_small= pygame.font.Font(FONTPATH, 18)
+	font_big= pygame.font.Font(FONTPATH, 24)
+	for num_level in range(1, Levels.NUMLEVELS+1):
+		if num_level== 1:
+			level= Levels.Level1()
+			is_clearance= startLevelGame(level, screen, font_small)
+			if num_level== Levels.NUMLEVELS:
+				showText(screen, font_big, is_clearance, True)
+			else:
+				showText(screen, font_big, is_clearance)
+
+
+if __name__ == '__main__':
+	main(initialize())
